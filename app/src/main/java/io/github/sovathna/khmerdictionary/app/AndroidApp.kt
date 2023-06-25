6f97c5d2710321f17185b6fe2b019a28714e0746
Sovathna Hong <hongsovathna@gmail.com>
2023-06-26 04:07:57 +0700

    add files

---
+++ app/src/main/java/io/github/sovathna/khmerdictionary/app/AndroidApp.kt
@@ -0,0 +1,21 @@
+package io.github.sovathna.khmerdictionary.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import io.github.sovathna.khmerdictionary.BuildConfig
+import timber.log.Timber
+
+@HiltAndroidApp
+class AndroidApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }else{
+            TODO("implement Timber release tree")
+        }
+    }
+
+}
