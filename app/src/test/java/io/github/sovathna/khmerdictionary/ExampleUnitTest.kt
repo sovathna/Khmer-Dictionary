2535852ae526f152f1dea1ae9e0a9df530fadaff
Sovathna Hong <hongsovathana@tax.gov.kh>
2023-07-10 16:39:37 +0700

    add test dependencies

--- app/src/test/java/io/github/sovathna/khmerdictionary/ExampleUnitTest.kt
+++ app/src/test/java/io/github/sovathna/khmerdictionary/ExampleUnitTest.kt
@@ -1,7 +1,9 @@
 package io.github.sovathna.khmerdictionary
 
+import android.content.Context
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.mockito.Mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,6 +11,8 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+  @Mock
+  private lateinit var mockContext:Context
   @Test
   fun addition_isCorrect() {
     assertEquals(4, 2 + 2)
